27f6c96d862a922a8fa6a63849a343320fb3d8bd
javed khan <javed.k2007@gmail.com>
2024-10-28 15:04:25 +0530

    intial commit

---
+++ models/listing.js
@@ -0,0 +1,26 @@
+const { ref } = require("joi");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+title : {
+    type:String,
+    required : true,
+},
+description: String,
+image :  {
+    type: String,
+    default: "https://plus.unsplash.com/premium_photo-1689609950112-d66095626efb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aG91c2V8ZW58MHx8MHx8fDA%3D",
+    set: (v) => v === "" ? "https://plus.unsplash.com/premium_photo-1689609950112-d66095626efb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aG91c2V8ZW58MHx8MHx8fDA%3D" : v
+},
+price : Number,
+location : String,
+country: String,
+reviews:[{
+    type:Schema.Types.ObjectId,
+    ref : "Review",
+}]
+});
+
+const Listing = mongoose.model("Listing" , listingSchema);
+module.exports = Listing;
